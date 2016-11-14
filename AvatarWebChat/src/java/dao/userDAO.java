198cdb375ecfbf619fee7462392bda203bd8a712
Kristoffer Svensson <ks222rt@student.lnu.se>
2016-11-14 15:54:05 +0100

    kept working on the dao class, but got an error about datasource not found

--- AvatarWebChat/src/java/dao/userDAO.java
+++ AvatarWebChat/src/java/dao/userDAO.java
@@ -5,10 +5,39 @@
  */
 package dao;
 
+import javax.sql.DataSource;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  *
  * @author Kristoffer
  */
 public class userDAO {
     
+    @Autowired
+    DataSource dataSource;
+    
+    private JdbcTemplate jdbcTemplate;
+    
+    public userDAO(){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    
+    public boolean addUser(User user){
+        String sql = "INSERT INTO WebChat.chat_user "
+                + "(username, firstname, lastname, email, password, salt) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        
+        
+        
+        int result;
+        result = jdbcTemplate.update(sql, 
+                new Object[] { user.getUsername(), user.getFirstname(),
+                    user.getLastname(), user.getEmail(),
+                    user.getPassword(), user.getSalt()});
+        
+        return result != 0;
+    }
 }
