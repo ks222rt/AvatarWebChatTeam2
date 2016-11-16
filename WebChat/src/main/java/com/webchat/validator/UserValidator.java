f9426df19abfa599adc54a2dc597492e12522358
Filip Rydberg <fr222cy@student.lnu.se>
2016-11-16 17:08:57 +0100

    Validator Class - empty body

---
+++ WebChat/src/main/java/com/webchat/validator/UserValidator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webchat.validator;
+
+import com.webchat.model.User;
+
+/**
+ *
+ * @author filip
+ */
+public class UserValidator {
+    
+    public boolean validateLoginAttempt(String username, String password){
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean validateRegisterAttempt(User user){
+        throw new UnsupportedOperationException();
+    }
+}
