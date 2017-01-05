68cfb33b107cd9648e7b7ab9dcb0e949a5447aee
ro222cm <ro222cm@student.lnu.se>
2017-01-05 17:36:53 +0100

    Added install script that should work on Ubuntu and Fedora.

---
+++ install-on-ubuntu/install-linux.sh
@@ -0,0 +1,82 @@
+#! /bin/sh
+# update & upgrade
+printf "Avatar WebChat Installation Script v 1.0\n\n"
+
+
+printf "Performing system update.\n"
+which apt-get > /dev/null 2> /dev/null
+if [ $? = 0 ]; then
+	sudo apt-get update
+	sudo apt-get upgrade
+	clear
+	sleep 2
+	printf "Installing dialog program..."
+	sudo dnf install dialog > /dev/null 2> /dev/null
+	printf "Done.\n"
+	sleep 1
+else
+	which dnf > /dev/null 2> /dev/null
+	if [ $? = 0 ]; then
+		sudo dnf check-update
+		sudo dnf update
+		clear
+		sleep 2
+		printf "Installing dialog program..."
+		sudo dnf install dialog > /dev/null 2> /dev/null
+		printf "Done.\n"
+		sleep 1
+	else
+		printf "This installation script uses apt-get (Debian, Ubuntu) or dnf (Fedora).\n"
+		exit 1
+	fi
+fi
+
+
+# install maven and Mysql
+dialog --title "Confirmation Required" --yesno "This will install the following required packages (if not already installed):\n * maven\n * mysql-server (or mariadb)\n\nContinue?" 12 40
+if [ $? != 0 ]; then
+	dialog --title "Important" --infobox "\nInstallation aborted" 5 25
+	sleep 3
+	dialog --clear
+	clear
+	exit 0
+fi
+
+which apt-get > /dev/null 2> /dev/null
+if [ $? = 0 ]; then
+	dialog --title "Installing Packages" --infobox "\nPlease wait." 5 25
+	sudo apt-get install maven > /dev/null 2> /dev/null
+	sudo apt-get install mysql-server > /dev/null 2> /dev/null
+	
+	# start mysql server
+	service mysqld restart
+	
+	dialog --title "Confirmation Required" --yesno "Do you want to run mysql_secure_installation now?" 12 40
+	if [ $? = 0 ]; then
+		clear
+		sudo mysql_secure_installation
+	fi
+else
+	dialog --title "Installing Packages" --infobox "\nPlease wait." 5 25
+	sudo dnf -y install mariadb mariadb-server > /dev/null 2> /dev/null
+	sudo dnf -y install maven > /dev/null 2> /dev/null
+	
+	# start mariadb server
+	sudo systemctl start mariadb
+	dialog --title "Confirmation Required" --yesno "Do you want to run mysql_secure_installation now?" 12 40
+	if [ $? = 0 ]; then
+		clear
+		sudo mysql_secure_installation
+	fi
+fi
+
+# log in to mysql db
+clear
+printf "Creating database tables. Logging in to mysql server as root\n"
+mysql -u root -p < avatar_webchat_db.sql
+
+dialog --title "Building Package" --infobox "\nPlease wait." 5 25
+cd ../WebChat
+mvn package > /dev/null 2> ../build_errors.log
+dialog --title "Installation Complete" --infobox "See build_error.log in the root directory for any errors that might have occured." 8 35
+
